88cbbf5b596a9f5afc63f8e357bc6b1d67315e1b
gary-ruizhang <gary.ruizhang@gmail.com>
2022-07-24 12:01:13 +0800

    Add files via upload

--- doom/config.el
+++ doom/config.el
@@ -1,119 +1,79 @@
-;; .doom.d/config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
 
-;;;;;;;;;;;;;;;;;;;;;;
-;;; Basic Settings ;;;
-;;;;;;;;;;;;;;;;;;;;;;
 
-;; delete trailing space after save
-(add-hook! 'before-save-hook 'delete-trailing-whitespace)
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Zhang Rui"
+      user-mail-address "gary.ruizhang@gmail.com")
 
-;; maximize screen on start up
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
-(setq-default indent-tabs-mode nil)
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-nord)
 
-(remove-hook 'tty-setup-hook 'doom-init-clipboard-in-tty-emacs-h)
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
 
-;;;;;;;;;;
-;;; UI ;;;
-;;;;;;;;;;
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
 
-(+global-word-wrap-mode t)
 
-;; emacs doesn't support ligature natively
-(setq doom-font (font-spec :family "Operator Mono SSm" :size 30))
-
-(load-theme 'doom-nord t)
-
-;;;;;;;;;;;;;
-;;; Shell ;;;
-;;;;;;;;;;;;;
-
-(setq eshell-cmpl-ignore-case t)
-
-;;;;;;;;;;;;;;;;;;;;
-;;; Key Mappings ;;;
-;;;;;;;;;;;;;;;;;;;;
-
-;; s map to evil-snipe by default in doom emacs, disable this to make
-;; s remapping work
-(after! evil-snipe
-  (evil-snipe-mode -1))
-
-;; ; -> ; when enter f start evil-snipe, otherwise ; -> :
-(map! :n ";" 'evil-ex
-      ;; use swiper to replace default search
-      ;; esc clear highlight when finish search
-      :n "/" 'swiper
-      :n "'" 'evil-goto-mark
-      :n "`" 'evil-goto-mark-line
-      ;; enable only when line wrap set
-      :n "j" 'evil-next-visual-line
-      :n "k" 'evil-previous-visual-line
-      :n "s" 'avy-goto-char-2
-      :gi "C-h" 'backward-delete-char)
-
-;;;;;;;;;;;;;;;;;;;
-;;; programming ;;;
-;;;;;;;;;;;;;;;;;;;
-
-; company-mode
-(setq company-idle-delay 0
-      company-minimum-prefix-length 2)
-
-;;;;;;;;;;;;;;;;
-;;; language ;;;
-;;;;;;;;;;;;;;;;
-
-; racket
-(setq racket-racket-program "/Applications/Racket v7.8/bin/racket")
-(setq racket-raco-program "/Applications/Racket v7.8/bin/raco")
-
-;;;;;;;;;;;;;;;
-;;; Package ;;;
-;;;;;;;;;;;;;;;
-
-; avy
-(setq avy-all-windows t)
-
-; evil
-
-; org-mode
-
-;; disable company in org
-(add-hook! 'org-mode-hook (company-mode -1))
-
-;; change the ...ellipsis that use to hidden content
-;; (setq org-ellipsis "...")
-
-;; org-bullets
-
-;; yinyang bullets
-;; (setq org-bullets-bullet-list '("☯" "☰" "☱" "☲" "☳" "☴" "☵" "☶" "☷"))
-
-;; org-todo
-(after! org
-  (setq org-todo-keywords '((sequence "TODO(t)" "INPROGRESS(i)" "|" "DONE(d)" "CANCELLED(c)"))
-        org-log-done 'time
-        org-agenda-files (directory-files-recursively "~/org/" "\.org$")
-        org-todo-keyword-faces '(("TODO" :foreground "#7c7c75" :weight normal :underline t )
-                                 ("INPROGRESS" :foreground "#0098dd" :weight normal :underline t)
-                                 ("DONE" :foreground "#50a14f" :weight normal :underline t)
-                                 ("CANCELLED" :foreground "#ff6480" :weight normal :underline t))))
-
-;; org-super-agenda
-(setq  org-super-agenda-groups '((:name "Today"
-                                  :time-grid t
-                                  :scheduled today)
-                                (:name "Due today"
-                                        :deadline today)
-                                (:name "Important"
-                                        :priority "A")
-                                (:name "Overdue"
-                                        :deadline past)
-                                (:name "Due soon"
-                                        :deadline future)
-                                (:name "Big Outcomes"
-                                        :tag "bo")))
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+(add-hook 'window-setup-hook #'toggle-frame-fullscreen)
+(setq doom-font (font-spec :family "OperatorMonoSSmLig Nerd Font" :size 26))
+(map! :n ";" 'evil-ex)
