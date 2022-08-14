5e561a9d55379f284a9185806fc742d70430a723
gary-ruizhang <gary.ruizhang@gmail.com>
2022-08-14 13:29:16 +0800

    Add files via upload

--- nvim/lua/lsp/cmp.lua
+++ nvim/lua/lsp/cmp.lua
@@ -9,12 +9,36 @@ local has_words_before = function()
 end
 
 cmp.setup({
-  -- gopls preselect for no fucking reasons
   preselect = cmp.PreselectMode.None,
 
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
   }, { { name = "buffer" }, { name = "path" } }),
+
+  snippet = {
+  -- We recommend using *actual* snippet engine.
+  -- It's a simple implementation so it might not work in some of the cases.
+    expand = function(args)
+      local line_num, col = unpack(vim.api.nvim_win_get_cursor(0))
+      local line_text = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, true)[1]
+      local indent = string.match(line_text, '^%s*')
+      local replace = vim.split(args.body, '\n', true)
+      local surround = string.match(line_text, '%S.*') or ''
+      local surround_end = surround:sub(col)
+
+      replace[1] = surround:sub(0, col - 1)..replace[1]
+      replace[#replace] = replace[#replace]..(#surround_end > 1 and ' ' or '')..surround_end
+      if indent ~= '' then
+        for i, line in ipairs(replace) do
+          replace[i] = indent..line
+        end
+      end
+
+      vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, replace)
+      -- just use for one line snippet, put position at the end
+      vim.api.nvim_win_set_cursor(0, {line_num, col + #replace[1]})
+    end,
+  },
 
   mapping = {
     ['<C-f>'] = cmp.mapping.confirm {
--- nvim/lua/lsp/setup.lua
+++ nvim/lua/lsp/setup.lua
@@ -1,15 +1,14 @@
 local nvim_lsp = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities(), { snippetSupport = false })
 
 -- TODO seperate each lsp to different config files
 -- TODO BUT I only want to write RUST in vim
-local servers = { 'rust_analyzer' }
+local servers = { 'rust_analyzer', 'sumneko_lua' }
 
 local opts = { silent=true }
 
 vim.keymap.set('n', 'gp', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', 'gn', vim.diagnostic.goto_next, opts)
-
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -20,7 +19,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'g[', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 end
 
@@ -31,3 +30,29 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+-- FIXME refactor lsp config in the future
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      completion = {
+        keywordSnippet = "Disable",
+      },
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
